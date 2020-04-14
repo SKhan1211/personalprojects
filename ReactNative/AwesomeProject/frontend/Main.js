6d2fd5e7e57719cf120784047d903e2c8f634638
Suhaib <suhaib92khan@gmail.com>
2020-04-13 23:58:36 -0400

    Create flex_direction component

--- ReactNative/AwesomeProject/frontend/Main.js
+++ ReactNative/AwesomeProject/frontend/Main.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LotsOfStyles from './style/style';
+import FixedDimensionBasics from './dimensions/fixed';
+import FlexDimensionsBasics from './dimensions/flex';
+import FlexDirectionBasics from './flex/flex_direction';
 
 export default function Main() {
   return (
-    <View style={styles.container}>
-      <Text>Hey!!</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Hey!!</Text>
+    // </View>
+    // <LotsOfStyles />
+    // <FixedDimensionBasics />
+    // <FlexDimensionsBasics />
+    <FlexDirectionBasics />
   );
 }
 
